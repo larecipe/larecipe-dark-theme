f63d1e3e1b323ef3b700197301de87a3d5845ed7
Saleem Hadad <saleem_ha@hotmail.com>
2019-04-01 02:09:44 +0800

    add source

---
+++ src/LaRecipeDarkThemeServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BinaryTorch\LaRecipeDarkTheme;
+
+use BinaryTorch\LaRecipe\LaRecipe;
+use Illuminate\Support\ServiceProvider;
+
+class LaRecipeDarkThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // LaRecipe::style('LaRecipeDarkTheme', __DIR__.'/../resources/css/dark.css');
+    }
+}
